7055a964d971dfdb07a65d51901158b6aa660098
vjalby <vincent.jalby@orange.fr>
2024-10-10 17:21:24 +0200

    first commit

---
+++ build.R
@@ -0,0 +1,13 @@
+install.packages('jmvtools', repos=c('https://repo.jamovi.org', 'https://cran.r-project.org'))
+options(jamovi_home='/Applications/jamovi')
+options(jamovi_home='/Applications//jamovi-x86_64')
+jmvtools::check()
+
+
+
+jmvtools::create('vijPlots') # Module Name
+
+jmvtools::addAnalysis(name='likertplot', title='Likert Plot') # name = function/files name, title = menu item name
+
+jmvtools::install()
+
